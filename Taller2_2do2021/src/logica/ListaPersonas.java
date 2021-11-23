735ea004a4d4f3492b112566f773d947148c5128
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-11-22 23:59:23 -0300

    avances contenedores

--- Taller2_2do2021/src/logica/ListaPersonas.java
+++ Taller2_2do2021/src/logica/ListaPersonas.java
@@ -1,5 +1,30 @@
 package logica;
+import dominio.*;
 
 public class ListaPersonas {
+	private int cant;
+	private int max;
+	private Persona [] listaPersonas;
+	
+	public ListaPersonas(int max) {
+		cant =0;
+		this.max = max;
+		listaPersonas = new Persona[max];
+	}
+	
+	public int getCant() {
+		return cant;
+	}
+	
+	public boolean ingresar(Persona persona) {
+		if(cant<max) {
+			listaPersonas[cant] = persona;
+			cant++;
+			return true;
+		}
+		return false;
+	}
+	
+	
 
 }
