123b0b091dfb9556866aa82bb99b83a3f83e4f81
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-11-23 23:30:16 -0300

    cambios

--- Taller2_2do2021/src/dominio/AsignaturaObligatoria.java
+++ Taller2_2do2021/src/dominio/AsignaturaObligatoria.java
@@ -5,6 +5,7 @@ import logica.*;
 public class AsignaturaObligatoria extends Asignatura{
 	
 	private int nivelMalla;
+	private int cantAsignaturasPrerrequisito;
 	private ListaAsignaturas listaAsignaturas;
 
 	public AsignaturaObligatoria(String codigoAsignatura, String nombreAsignatura, int cantCreditos,int nivelMalla,int cantAsignaturasPrerrequisito) {
@@ -19,6 +20,15 @@ public class AsignaturaObligatoria extends Asignatura{
 
 	public void setNivelMalla(int nivelMalla) {
 		this.nivelMalla = nivelMalla;
+	}
+	
+
+	public int getCantAsignaturasPrerrequisito() {
+		return cantAsignaturasPrerrequisito;
+	}
+
+	public void setCantAsignaturasPrerrequisito(int cantAsignaturasPrerrequisito) {
+		this.cantAsignaturasPrerrequisito = cantAsignaturasPrerrequisito;
 	}
 
 	public ListaAsignaturas getListaAsignaturas() {
